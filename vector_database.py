483dcaf06258c7bc24f0e26c1fe2edb3c05d0890
Ignacio <ignaji01@ucm.es>
2024-03-20 19:01:23 +0100

    Vector Database actualizada para aceptar varios archivos

--- vector_database.py
+++ vector_database.py
@@ -3,7 +3,7 @@ from qdrant_client import QdrantClient
 from qdrant_client.http.models import Distance, VectorParams, PointStruct
 from qdrant_client.http import models
 import time
-from wiki_dataset import WikiDataset
+import os
 
 distances = {
     'mpnet': Distance.DOT,
@@ -12,17 +12,17 @@ distances = {
 
 class VectorDatabase():
 
-    def __init__(self, client='local', ip='localhost', port=6333, encoder='mpnet', version='v1'):
+    def __init__(self, client='local', ip='localhost', port=6333, encoder='mpnet', version='v1', wiki=None):
 
         self.collection = f'{encoder}_{version}'
         if client == 'local':
             self.client = QdrantClient(':memory:')
-            self.start_collection()
+            self.start_collection(wiki)
 
         elif client == 'docker':
             self.client = QdrantClient(f'{ip}:{port}')
             if self.collection not in [c.name for c in self.client.get_collections().collections]:
-                self.start_collection()
+                self.start_collection(wiki)
         else:
             raise Exception('Invalid client type')
         
@@ -38,32 +38,34 @@ class VectorDatabase():
         return self.client.search_batch(collection_name=self.collection, requests=search_queries)
         
 
-    def start_collection(self):
+    def start_collection(self, wiki):
         init = time.time()
         print('Creating collection')
-        data, vector_size = self.loadData()
-        print('Data loaded')
-        self.client.create_collection(
-            collection_name=self.collection,
-            vectors_config=VectorParams(size=vector_size, distance=Distance.DOT),
-        )
-        self.client.upload_points(
-            collection_name=self.collection,
-            points=data,
-            parallel=4,
-            max_retries=3,
-        )
+        # count the number of files in folder embeddings
+        for i, f in enumerate(os.listdir('embeddings')):
+            data, vector_size = self.loadData(i, f, wiki)
+            if i == 0:
+                self.client.create_collection(
+                    collection_name=self.collection,
+                    vectors_config=VectorParams(size=vector_size, distance=Distance.DOT),
+                )
+            self.client.upload_points(
+                collection_name=self.collection,
+                points=data,
+                parallel=4,
+                max_retries=3,
+            )
         print('Time:', time.time() - init)
 
         
-    def loadData(self, wiki):
-        embeddings = np.load(f'embeddings/{self.collection}.npy')
+    def loadData(self, i, file, wiki):
+        embeddings = np.load(f'embeddings/{file}')
         vector_size = embeddings.shape[1]
         data = []
-        for i in range(len(wiki)):
-            page = wiki[i]
+        for j in range(embeddings.shape[0]):
+            page = wiki[i*embeddings.shape[0] + j]
             id, text, lines = page['id'], page['text'], page['lines']
-            ps = PointStruct(id=id, vector=embeddings[i].tolist(), payload={"text": text, "lines": lines})
+            ps = PointStruct(id=id, vector=embeddings[j].tolist(), payload={"text": text, "lines": lines})
             data.append(ps)
         return data, vector_size
 
